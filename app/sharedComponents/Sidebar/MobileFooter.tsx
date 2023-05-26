c7c506a7b0c57dbab2dff6c4680c0e90a128b966
Quangliem59 <liemh172@gmail.com>
2023-05-26 16:18:53 +0700

    add setting

--- app/sharedComponents/Sidebar/MobileFooter.tsx
+++ app/sharedComponents/Sidebar/MobileFooter.tsx
@@ -6,11 +6,19 @@ import DesktopItem from "./DesktopItem";
 import useConversation from "@/app/hooks/useConversation";
 import MobileItem from "./MobileItem";
 import LoadingModal from "../modals/LoadingModal";
+import SettingsModal from "./SettingsModal";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar/Avatar";
 
-const MobileFooter = () => {
+interface MobileFooterProps {
+  currentUser: User;
+}
+
+const MobileFooter: React.FC<MobileFooterProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const { isOpen } = useConversation();
   const [isLoading, setIsLoading] = useState(false);
+  const [isOpenSettingModal, setIsOpenSettingModal] = useState(false);
 
   const handleItemClick = (onClick: any) => {
     setIsLoading(true);
@@ -30,12 +38,23 @@ const MobileFooter = () => {
   return (
     <>
       {isLoading && <LoadingModal />}
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpenSettingModal}
+        onClose={() => setIsOpenSettingModal(false)}
+      />
       <div className="fixed flex justify-between w-full bottom-0 z-40 items-center bg-white border-t-[1px] lg:hidden ">
-        <nav className="flex items-center justify-between w-full py-2">
+        <nav className="flex items-center justify-between flex-1 w-full py-2">
           <ul
             role="list"
             className="flex items-center justify-around w-full space-y-1 "
           >
+            <li
+              onClick={() => setIsOpenSettingModal(true)}
+              className="transition cursor-pointer hover:opacity-75"
+            >
+              <Avatar user={currentUser} />
+            </li>
             {routes.map((item) => (
               <MobileItem
                 key={item.label}
