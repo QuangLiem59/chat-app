d4dfe55fb8bfaefc05b02cc86c31e693d5cc286f
Quangliem59 <liemh172@gmail.com>
2023-05-26 16:48:15 +0700

    fix something

--- app/sharedComponents/Avatar/AvatarGroup.tsx
+++ app/sharedComponents/Avatar/AvatarGroup.tsx
@@ -11,13 +11,13 @@ const AvatarGroup: React.FC<AvatarGroupProps> = ({ users = [] }) => {
   const slicedUsers = users.slice(0, 3);
 
   const positionMap = {
-    0: "top-0 left-[12px]",
-    1: "bottom-0",
-    2: "bottom-0 right-0",
+    0: "top-0 left-[10px]",
+    1: "bottom-1 left-[2px]",
+    2: "bottom-1 right-[2px]",
   };
 
   return (
-    <div className="relative h-11 w-11">
+    <div className="relative bg-gray-200 border rounded-full shadow md:h-11 md:w-11 h-9 w-9">
       {slicedUsers.reverse().map((user, index) => (
         <div
           key={user.id}
@@ -26,8 +26,10 @@ const AvatarGroup: React.FC<AvatarGroupProps> = ({ users = [] }) => {
             inline-block 
             rounded-full 
             overflow-hidden
-            h-[21px]
-            w-[21px]
+            md:h-[19px]
+            md:w-[19px]
+            h-[15px]
+            w-[15px]
             ${positionMap[index as keyof typeof positionMap]}
           `}
         >
@@ -35,7 +37,7 @@ const AvatarGroup: React.FC<AvatarGroupProps> = ({ users = [] }) => {
             fill
             src={user?.image || "/images/avatar.png"}
             alt="Avatar"
-            sizes={"21px, 21px"}
+            sizes={"(min-width: 640px) 19px, 15px"}
             className="object-contain"
           />
           {/* <img
